5f5a85e2f7b57ebba2f3003b1868f06d7986bc91
Roman Iackobchuk <kaboom.ua@gmail.com>
2015-03-12 17:14:12 +0200

    Bacon demo

--- app/main.js
+++ app/main.js
@@ -1,7 +1,44 @@
-$(function(){
-    var name = Bacon.fromEvent(document.getElementById('myInp'), 'keyup')
-        .map(function(ev) {
-            return ev.currentTarget.value
-        }).toProperty('');
-    name.log('name is ');
+//Я Тут додав трохи jQuery, просто щоб зменшити код і показати як Bacon інтегрується з ним
+
+$(function () {
+    var element = document.getElementById('response'),
+        buttons = $('button');
+    var targetText = Bacon
+        .fromEventTarget(document, 'mousemove')
+        .filter(targetContainsText)
+        .map(getTextFromEvent)
+        //А можна й так, Бекон викличе ці методи на об'єкті події:
+        //.map('.target.textContent')
+        .skipDuplicates();
+    //А от і приклад: asEventStream - метод, який Bacon додає jQuery колекціям
+    var buttonPresses = buttons.asEventStream('click')
+        .map(processButtonClick);
+
+    var responses = targetText.merge(buttonPresses).flatMapLatest(postText);
+
+    responses.onValue(function(response) {
+        element.innerHTML = response
+    })
 });
+
+function postText(text) {
+    $.mockjax.clear();
+    $.mockjax({
+        url: "/text",
+        responseTime: Math.random()*2000,
+        response: function(req) {
+            this.responseText =  'server: ' + req.data;
+        }
+    });
+    return Bacon.fromPromise($.post('/text', text))
+}
+
+function targetContainsText(ev) {
+    return ev.target.childNodes.length == 1 && ev.target.childNodes[0].nodeType == 3
+}
+function getTextFromEvent(ev) {
+    return ev.target.textContent
+}
+function processButtonClick(ev) {
+    return 'Our dear user has clicked the ' + ev.currentTarget.value + ' button'
+}
